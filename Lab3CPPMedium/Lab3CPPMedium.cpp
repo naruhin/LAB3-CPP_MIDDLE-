a0ceabbf1530e8fafee45b3e52188c675467e781
Дима Толкунов <tolkunoff.dima@yandex.ru>
2019-04-15 20:42:43 +0300

    Добавьте файлы проекта.

---
+++ Lab3CPPMedium/Lab3CPPMedium.cpp
@@ -0,0 +1,43 @@
+﻿//Вариант 17
+//Введите 5 массивов символов длиной 8 элементов.
+//Расположить строки в алфавитном порядке (как в словаре)
+#include <iostream>
+#include <Windows.H>
+
+using namespace std;
+
+void sortByAlphabet(char** arr, int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++)
+			if (strcmp(arr[i], arr[j]) > 0) {
+				char* tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+	}
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const int N = 5;
+
+	char** arr = new char* [N];
+
+	cout << "Введите 5 слов : " << endl;
+	for (int i = 0; i < N; i++) {
+		arr[i] = new char[8];
+		cin >> arr[i];
+	}
+	
+	
+	sortByAlphabet(arr, N);
+	cout << "Сортированный массив : " << endl;
+	for (int i = 0; i < N; i++) {
+		cout << "\t" << arr[i] << endl;
+	}
+}
+
+
+
